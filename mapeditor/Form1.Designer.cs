62f88e34445389e2dda03130ba3ca1c42f7e5eec
平 加納 <s153405@stn.nagaokaut.ac.jp>
2016-03-03 21:16:33 +0900

    マップエディタ画像加工機能追加

--- mapeditor/Form1.Designer.cs
+++ mapeditor/Form1.Designer.cs
@@ -40,51 +40,57 @@
             // 
             // button_waku
             // 
-            this.button_waku.Location = new System.Drawing.Point(35, 118);
+            this.button_waku.Location = new System.Drawing.Point(47, 148);
+            this.button_waku.Margin = new System.Windows.Forms.Padding(4);
             this.button_waku.Name = "button_waku";
-            this.button_waku.Size = new System.Drawing.Size(97, 30);
+            this.button_waku.Size = new System.Drawing.Size(129, 38);
             this.button_waku.TabIndex = 0;
-            this.button_waku.Text = "枠作製";
+            this.button_waku.Text = "v";
             this.button_waku.UseVisualStyleBackColor = true;
             this.button_waku.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(35, 57);
+            this.textBox1.Location = new System.Drawing.Point(47, 71);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(73, 19);
+            this.textBox1.Size = new System.Drawing.Size(96, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(35, 82);
+            this.textBox2.Location = new System.Drawing.Point(47, 102);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(71, 19);
+            this.textBox2.Size = new System.Drawing.Size(93, 22);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Location = new System.Drawing.Point(16, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.Size = new System.Drawing.Size(22, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "縦";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Location = new System.Drawing.Point(16, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.Size = new System.Drawing.Size(22, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "横";
             // 
             // button_imgRead
             // 
-            this.button_imgRead.Location = new System.Drawing.Point(35, 263);
+            this.button_imgRead.Location = new System.Drawing.Point(47, 329);
+            this.button_imgRead.Margin = new System.Windows.Forms.Padding(4);
             this.button_imgRead.Name = "button_imgRead";
-            this.button_imgRead.Size = new System.Drawing.Size(97, 30);
+            this.button_imgRead.Size = new System.Drawing.Size(129, 38);
             this.button_imgRead.TabIndex = 5;
             this.button_imgRead.Text = "画像読み取り";
             this.button_imgRead.UseVisualStyleBackColor = true;
@@ -93,26 +99,28 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(36, 322);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(48, 402);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(95, 244);
+            this.listBox1.Size = new System.Drawing.Size(125, 304);
             this.listBox1.TabIndex = 6;
             // 
             // button_mapExport
             // 
-            this.button_mapExport.Location = new System.Drawing.Point(36, 194);
+            this.button_mapExport.Location = new System.Drawing.Point(48, 246);
+            this.button_mapExport.Margin = new System.Windows.Forms.Padding(4);
             this.button_mapExport.Name = "button_mapExport";
-            this.button_mapExport.Size = new System.Drawing.Size(97, 30);
+            this.button_mapExport.Size = new System.Drawing.Size(129, 38);
             this.button_mapExport.TabIndex = 7;
             this.button_mapExport.Text = "map出力";
             this.button_mapExport.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1197, 615);
+            this.ClientSize = new System.Drawing.Size(1596, 769);
             this.Controls.Add(this.button_mapExport);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button_imgRead);
@@ -121,8 +129,9 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button_waku);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "v";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- mapeditor/Form1.cs
+++ mapeditor/Form1.cs
@@ -14,6 +14,7 @@ namespace mapeditor
 
     public partial class Form1 : Form
     {
+        Size tip_size = new Size(32, 32);
         public Form1()
         {
             InitializeComponent();
@@ -26,22 +27,24 @@ namespace mapeditor
             int tate = int.Parse(textBox1.Text);
             int yoko = int.Parse(textBox2.Text);
             button_dipose(yoko,tate,this);
-            
+
         }
         private void addImage(object sender, EventArgs e)
         {
             int buttonId = ((Button)sender).TabIndex-1; // ボタンの名前
-            string imagepath = dict[listBox1.SelectedItem.ToString()];
-            System.Drawing.Image img = System.Drawing.Image.FromFile(imagepath);
+            string imagepath = listBox1.SelectedItem.ToString();
+            Bitmap img = img_list[imagepath];
             list[buttonId].Image = img;
-        }
+            Console.WriteLine(list[buttonId].Size);
+            
+       }
 
         List<Button> list = new List<Button>();
         public void button_dipose(int width, int height, Form f)
         {
             
             // 初期宣言
-            Size s = new Size(32, 32);
+            Size s = new Size(38, 38);
             int b_num;
             if (list.Count != 0)
             {
@@ -61,21 +64,19 @@ namespace mapeditor
                 {
                     Button btn = new Button();
                     b_num++;
-
                     // 2-1)インスタンスを作成
                     // 2-2)配置位置を設定
                     btn.Location = new Point(150 + s.Width * (i % width), 150 + s.Height * (j % height));
-                    //Console.WriteLine("i % {1} = {0}",i%width, width); 
-                    //Console.WriteLine("i % {1} = {0}", i / height, height);
-                    Console.WriteLine("({0},{1})", i % width, i / height);
+                    btn.AutoSize = true;
                     // 2-3)Nameプロパティを設定
                     btn.Name = "Button" + i;
+
                     // 2-4)サイズを設定
                     btn.Size = new System.Drawing.Size(s.Width, s.Height);
                     // 2-5)TabIndexを設定
                     btn.TabIndex = b_num;
                     // 2-6)ボタンテキストを設定
-                    btn.Text = btn.TabIndex.ToString();
+                    btn.Text = i + "," + j;
                     list.Add(btn);
                     btn.Click += addImage;
                 }
@@ -99,22 +100,106 @@ namespace mapeditor
         {
             OpenFileDialog ofd = new OpenFileDialog();
             string tmppath;
-            if(ofd.ShowDialog() == DialogResult.OK)
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
                 tmppath = System.IO.Path.GetFileName(ofd.FileName);
                 dict.Add(tmppath, ofd.FileName);
-                listBox1.Items.Add(tmppath);
+                
+             
+                split_img_2(ofd.FileName);
+
+                foreach (var i in img_list)
+                {
+                    listBox1.Items.Add(i.Key);
+                }
+            }
+            
+        }
+        Dictionary<String, Bitmap> img_list;
+
+        private void split_img_1(String path)
+        {
+            Bitmap img_base;
+            Bitmap img_tip;
+            img_list = new Dictionary<string, Bitmap>();
+            int tip_count = 0;
+            // 画像を切り抜く
+            if (path != null)
+            {
+
+                img_base = new Bitmap(path);
+                string img_name = path.Split('\\')[path.Split('\\').GetUpperBound(0)];
+                Console.WriteLine(img_name);
+                int i = 0, j = 0;
+                while (i <= img_base.Size.Width - tip_size.Width)
+                {
+                    while (j <= img_base.Size.Height - tip_size.Height)
+                    {
+                        Rectangle rect = new Rectangle(i, j, tip_size.Width, tip_size.Height);
+                        img_tip = img_base.Clone(rect, img_base.PixelFormat);
+                        img_list.Add(img_name + "_" + tip_count, img_tip);
+
+                        tip_count++;
+                        Console.WriteLine(img_name + "_" + tip_count);
+                        if (j + tip_size.Height * 2 <= img_base.Size.Height)
+                            j += tip_size.Height;
+                        else
+                            j += j + tip_size.Height - img_base.Size.Height;
+                    }
+                    i += tip_size.Width;
+                }
+
+                img_base.Dispose();
             }
         }
-    }
 
-
-
-    class imagebutton : Button
-    {
-        private void editbutton()
+        private void split_img_2(String path)
         {
+            Bitmap img_base;
+            Bitmap img_back = new Bitmap(@"C:\Users\s1534\Pictures\background.bmp");
+            Bitmap img_tip;
 
+            img_list = new Dictionary<string, Bitmap>();
+            int tip_count = 0;
+            // 画像を切り抜く
+            if (path != null)
+            {
+
+                img_base = new Bitmap(path);
+                string img_name = path.Split('\\')[path.Split('\\').GetUpperBound(0)];
+                Console.WriteLine(img_name);
+                Console.WriteLine(img_base.Size);
+                int i = 0;
+                do
+                {
+                    int j = 0;
+                    while (j <= img_base.Size.Height - tip_size.Height)
+                    {
+                        Rectangle rect = new Rectangle(i, j, tip_size.Width, tip_size.Height);
+                        img_tip = img_base.Clone(rect, img_base.PixelFormat);
+                        img_list.Add(img_name + "_" + tip_count, img_tip);
+
+                        tip_count++;
+                        Console.WriteLine(img_name + "_" + tip_count);
+                        j += tip_size.Height;
+                        if (j + tip_size.Height * 2 > img_base.Size.Height)
+                            j -= j + tip_size.Height - img_base.Size.Height;
+                    }
+                    i += tip_size.Width;
+                    if (i + tip_size.Width * 2 > img_base.Size.Width)
+                        i -= i + tip_size.Width - img_base.Size.Width;
+
+                } while (i <= img_base.Size.Width - tip_size.Width);
+
+                img_base.Dispose();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (img_list != null)
+                foreach (var i in img_list)
+                    i.Value.Dispose();
         }
     }
 }
